a1443aa9317c2c17af9d43d994bf1362f863d5b8
alan8r <alan8r@gmail.com>
2023-11-27 17:59:54 -0800

    broken but loads, sort of stable, positioning still broken also

--- admindash/src/DashMain.js
+++ admindash/src/DashMain.js
@@ -36,44 +36,40 @@ let uriGet = 'https://localhost:5500/music/songs',
     apiData = fetch(uriGet).then(async response=>console.log(await response.json()));
 */
 
+function populateDataTable(data) {
+    let table = document.querySelector("table");
+
+
+    function addCell(tr, text) {
+        let td = tr.insertCell()
+        td.textContent = text
+        return td
+    }
+
+    data.forEach(item => {
+        let row = table.insertRow()
+        addCell(row, item.key)
+        addCell(row, item.title)
+        addCell(row, item.game)
+    })
+}
 
 
 function DashMain() {
-
-    function populateDataTable(data) {
-        let table = document.querySelector("table")
-
-        if (!table) return;
     
-        function addCell(tr, text) {
-            let td = tr.insertCell()
-            td.textContent = text
-            return td
-        }
-    
-        data.forEach(item => {
-            let row = table.insertRow()
-            addCell(row, item.key)
-            addCell(row, item.title)
-            addCell(row, item.game)
-        })
-    }
-
     return (
         <div id='container'>
             <h1>Admin dashboard</h1>
             <div id='dataAndFilter'>
-                <input type='search' id='filter'></input>
-                <div class='tableFixHead'>
+                <input type='search' id='filter' placeholder='filter'></input>
+                <div className='tableFixHead'>
                     <table >
                         <thead>
                             <th>Index</th>
                             <th>Title</th>
                             <th>Game</th>
                         </thead>
-                        <tbody>
-                        
-                        </tbody>
+                        <tbody></tbody>
                     </table>
                 </div>
             </div>
@@ -81,7 +77,6 @@ function DashMain() {
                 <button id='btnAddSong' disabled>Add song</button>
                 <button id='btnEditSong' disabled>Edit song</button>
             </div>
-            {populateDataTable(fakeData)}
         </div>
     )
 }
