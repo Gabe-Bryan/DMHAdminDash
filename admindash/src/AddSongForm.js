f09c8a3743ff4b7a808a335af7922d4e199138f7
Gabe-Bryan <gabriel.bryan544@gmail.com>
2023-12-01 20:26:46 -0800

    added the basic source form

--- admindash/src/AddSongForm.js
+++ admindash/src/AddSongForm.js
@@ -1,12 +1,14 @@
 import React from "react";
-import {Button, Layout, Input, Space, ConfigProvider, theme,Form} from "antd";
+import {Button, Layout, Input, Space, ConfigProvider, theme,Form, Card} from "antd";
 import { Content } from "antd/es/layout/layout";
 import { getSongs, getAllSongs, addNewSongTitleSimple, addNewSongURL } from './APICalls';
+import {CloseOutlined} from '@ant-design/icons';
 
 const onFinish = (values) => {
   //console.log('Success:', values);
   //sconsole.log({title:values.song_title, soundtrack_id:null, meta_data: {lead_composer:values.lead_composer}, api_key:values.api_key})
   //getAllSongs();
+  //console.log(values);
   console.log({lead_composer:values.lead_composer, game:1, release_year:2021})
   console.log(addNewSongTitleSimple(values.song_title,null,{lead_composer:values.lead_composer, game:1, release_year:2021},values.api_key))
 };
@@ -110,8 +112,49 @@ function AddSongForm(){
         <Button htmlType="submit" >Enter Song and Sources Into Database</Button>
         </Space>
         </div>
+        <SourceForm/>
         </Form>
         </ConfigProvider>
     )
 }
+
+
+function SourceForm () {
+  return (
+  
+  <Card title = 'Sources'>
+    <Form.List name = 'sources'>
+      {(fields, {add, remove}) => (
+        <div style={{ display: 'flex', flexDirection: 'column', rowGap: 16 }}>
+          {fields.map((field) => (
+            <Space key = {field.key}>
+
+              <Form.Item required label = 'Video Id' name = {[field.name, 'video_id']}>
+                <Input ></Input>
+              </Form.Item>
+              <Form.Item required label = 'Source Type' name = {[field.name, 'source_type']}>
+                <Input ></Input>
+              </Form.Item>
+              <Form.Item label = 'Alternative Theme' name = {[field.name, 'alt_theme']}>
+                <Input ></Input>
+              </Form.Item>
+              <Form.Item label = 'Official Title' name = {[field.name, 'official_title']}>
+                <Input ></Input>
+              </Form.Item>
+              <center>
+                <CloseOutlined onClick = {() => remove(field.name)}/>
+              </center>
+            </Space>
+          ))}
+
+          
+          <Button type="default" onClick={add} >Add New Source</Button>
+        </div>)
+        
+      }
+    </Form.List>
+  </Card>
+  )
+}
+
 export default AddSongForm
